525b110387df1442113ed89d0ff9191c111a0702
Parathan Vakeesan <parathan243@gmail.com>
2020-05-13 22:59:52 -0400

    Created login and registration interface and functionality.

--- OrderPizza/views.py
+++ OrderPizza/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def index(request):
+    return render(request, "PizzaTemplates/Index.html")
+
+def register(request):
+    return render(request, "PizzaTemplates/Register.html")
+
+def loginview(request):
+    return render(request, "PizzaTemplates/Login.html")
+
+def registered(request):
+    FirstName = request.POST["FirstName"]
+    LastName = request.POST["LastName"]
+    Email = request.POST["Email"]
+    Username = request.POST["Username"]
+    Password = request.POST["Password"]
+    Passwordconfirm = request.POST["Password2"]
+    if Password != Passwordconfirm:
+        raise Http404("Passwords do not match")
+    user = User.objects.create_user(Username, Email, Password)
+    user.first_name = FirstName
+    user.last_name = LastName
+    user.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def loginverify(request):
+    Username = request.POST["username"]
+    Password = request.POST["password"]
+    user = authenticate(request, username=Username, password=Password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse('index'))
+    else:
+        raise Http404("Invalid Credentials")
