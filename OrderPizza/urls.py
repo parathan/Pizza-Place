674e95c5a74e2e02ae27430eab149234d7cd90ee
Parathan Vakeesan <parathan243@gmail.com>
2020-05-23 00:23:53 -0400

    Implementing urls for api

--- OrderPizza/urls.py
+++ OrderPizza/urls.py
@@ -1,6 +1,18 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
+from rest_framework import routers
+
+router = router.DefaultRouter()
+router.register(r'Regular', views.RegularViewSet)
+router.register(r'Sicilian', views.SicilianViewSet)
+router.register(r'Toppings', views.ToppingsViewSet)
+router.register(r'Subs', views.SubsViewSet)
+router.register(r'SubToppings', views.SubToppingsViewSet)
+router.register(r'Pasta', views.PastaViewSet)
+router.register(r'Salads', views.SaladsViewSet)
+router.register(r'Platters', views.PlattersViewSet)
 
 urlpatterns = [
-    
+    path("", include(router.url)),
+    path("api-auth/", include('rest_framework.urls', namespace = 'rest_framework')),
 ]
